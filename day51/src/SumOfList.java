8caa1cd90ad7cad07b2a962d899a38104a21d1a7
Mark Schledzewski <mark.schledzewski@gmail.com>
2022-05-09 15:02:25 -0400

    added a method

--- day51/src/SumOfList.java
+++ day51/src/SumOfList.java
@@ -5,7 +5,6 @@ public class SumOfList {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<Integer>();
-        int total = 0;
         for (int i = 0; i < list.size() + 1; i++) {
             int item = scanner.nextInt();
             if (item == 0) {
@@ -14,22 +13,27 @@ public class SumOfList {
                         System.out.print(list.get(j) + ", ");
                     } else {
                         System.out.println("and " + list.get(j) + " were the numbers you entered.");
+                        sum(list);
                     }
                 }
-                for (int q = 0; q < list.size(); q++) {
-                    total += list.get(q);
-                }
-                System.out.println("The sum of all these is: " + total);
-                System.out.println("What number are you looking for?");
-                int x = scanner.nextInt();
-                for (int z = 0; z < list.size(); z++) {
-                    if (list.get(z) == x) {
-                        System.out.println(x + " is at index " + z);
-                    }
-                }
-
             } else {
                 list.add(item);
+            }
+        }
+    }
+
+    public static void sum(ArrayList<Integer> list) {
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        for (int q = 0; q < list.size(); q++) {
+            total += list.get(q);
+        }
+        System.out.println("The sum of all these is: " + total);
+        System.out.println("What number are you looking for?");
+        int x = scanner.nextInt();
+        for (int z = 0; z < list.size(); z++) {
+            if (list.get(z) == x) {
+                System.out.println(x + " is at index " + z);
             }
         }
     }
